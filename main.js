87795adf9e8e0a28ed58bdd133163331efc97246
Adrian Gomes <adriangomes@Hugos-iMac.local>
2015-09-27 10:12:15 -0400

    added svg animation
    still faulty; may not work on all browsers and devices

---
+++ main.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+    $(this).scrollTop(0);
+    setTimeout(function(){
+        $( "body" ).addClass( "filled" )
+
+        $("body").css({
+            'overflow' : ''
+        });
+
+        $(this).scrollTop(0);
+
+        $(window).scroll(function(){
+            var windowScroll = $(this).scrollTop();
+            var stop = 250;
+
+            if (windowScroll < stop){
+                $("#t").css({
+                    "transform" : "translate( 0% , " + windowScroll * -1.5  + "%)"
+                });
+                $("#h").css({
+                    "transform" : "translate( 0% , " + windowScroll * -1.4  + "%)"
+                });
+                $("#i").css({
+                    "transform" : "translate( 0% , " + windowScroll * -1.3  + "%)"
+                });
+                $("#iDot").css({
+                    "transform" : "translate( 0% , " + windowScroll * -6.4  + "%)"
+                });
+                $("#n").css({
+                    "transform" : "translate( 0% , " + windowScroll * -1.5  + "%)"
+                });
+                $("#g").css({
+                    "transform" : "translate( 0% , " + windowScroll * -1.4  + "%)"
+                });
+                $("#ellipsis-1").css({
+                    "transform" : "translate( 0% , " + windowScroll * -13.3  + "%)"
+                });
+                $("#ellipsis-2").css({
+                    "transform" : "translate( 0% , " + windowScroll * -14.4  + "%)"
+                });
+                $("#ellipsis-3").css({
+                    "transform" : "translate( 0% , " + windowScroll * -15.5  + "%)"
+                });
+
+            }else {
+                $("#t").css({
+                    "transform" : "translate( 0% , " + stop * -1.5  + "%)"
+                });
+                $("#h").css({
+                    "transform" : "translate( 0% , " + stop * -1.4  + "%)"
+                });
+                $("#i").css({
+                    "transform" : "translate( 0% , " + stop * -1.3  + "%)"
+                });
+                $("#iDot").css({
+                    "transform" : "translate( 0% , " + stop * -6.4  + "%)"
+                });
+                $("#n").css({
+                    "transform" : "translate( 0% , " + stop * -1.5  + "%)"
+                });
+                $("#g").css({
+                    "transform" : "translate( 0% , " + stop * -1.4  + "%)"
+                });
+                $("#ellipsis-1").css({
+                    "transform" : "translate( 0% , " + stop * -13.3  + "%)"
+                });
+                $("#ellipsis-2").css({
+                    "transform" : "translate( 0% , " + stop * -14.4  + "%)"
+                });
+                $("#ellipsis-3").css({
+                    "transform" : "translate( 0% , " + stop * -15.5  + "%)"
+                });
+                $(this).scrollTop(stop);
+                $("body").css({
+                    "overflow" : "hidden"
+                });
+            }
+
+            if (windowScroll === stop){
+                $(".sub-head").css({
+                    "transform" : "translateY( -30%)"
+                });
+                $("body").addClass("check");
+            }
+
+        });
+    }, 3000);
+
+});
