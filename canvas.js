b3f687a9a9ec10c7a0b7188059ba45c71e358175
ahgomes <adrianhgomes@gmail.com>
2017-06-23 13:23:09 -0400

    Added canvas animation

---
+++ canvas.js
@@ -0,0 +1,67 @@
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+addEventListener('resize',  function() {
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+
+	init();
+});
+
+function rand(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randColor() {
+    var colors = ['rgba(255,255,255,.5)','rgba(0,0,0,.35)'];
+	return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function Circle(x, y, r, color) {
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this.color = color;
+
+	this.update = function() {
+        if(this.y < -this.r ) {
+            this.x = rand(this.r, canvas.width - this.r);
+            this.y = canvas.height + this.r;
+        }
+        this.x += rand(-1,1);
+        this.y += rand(-50,2);
+		this.draw();
+	};
+
+	this.draw = function() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+		c.lineWidth = 10;
+		c.strokeStyle = this.color;
+		c.stroke();
+		c.closePath();
+	};
+}
+
+var circles;
+function init() {
+    circles = [];
+    for(var i = 0; i < 100; i++) {
+        var r  = rand(5, 25);
+        var x  = rand(r, canvas.width - r);
+        var y  = rand(0, canvas.height - r);
+        circles.push(new Circle(x, y, r, randColor()));
+    }
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	c.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < circles.length; i++) circles[i].update();
+}
+
+init();
+animate();
